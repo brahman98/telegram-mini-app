3a1c9c46756ba6ffb103914fadb1872fdf115f10
brahman98 <bergen.bargen@gmail.com>
2024-12-17 17:26:34 +0300

    added block information

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import List from './components/List'
 import Modal from './components/Modal'
+import './App.css';
 
 type Item = {
   id: number;
@@ -24,7 +25,65 @@ function App() {
 
   return (
     <div>
-      <h1>Список объектов</h1>
+      <section className="energy">
+        <div className='energy__text'>
+          <p>Energy</p>
+          <p>950 / 1000 xp</p>
+        </div>
+        <div className="energy__range">
+          <div className="energy__range_green"></div>
+          <div className="energy__range_gray"></div>
+        </div>
+      </section>
+
+      <h1>Information</h1>
+      <section className="info">
+          <div className='info__part'>
+            <div className='info__part_key'>
+              <p className='margin-bottom'>Block</p> 
+              <p>Reward</p>
+            </div>
+            <div className='info__part_value'>
+              <p className='margin-bottom'>12345</p> 
+              <p>624</p>
+            </div>
+          </div>
+          <div className='info__part'>
+            <div className='info__part_key2'>
+              <p className='margin-bottom'>Complexity</p> 
+              <p>Online</p>
+            </div>
+            <div className='info__part_value'>
+              <p className='margin-bottom'>1.9K</p> 
+              <p>10K</p>
+            </div>
+          </div>
+      </section>
+      <section className="info">
+          <div className='info__part'>
+            <div className='info__part_key'>
+              <p className='margin-bottom'>Status</p> 
+              <p>Hashes</p>
+            </div>
+            <div className='info__part_value'>
+              <p className='margin-bottom special_p'>Waiting</p> 
+              <p>0</p>
+            </div>
+          </div>
+          <div className='info__part'>
+            <div className='info__part_key2'>
+              <p className='margin-bottom'>Shares</p> 
+              <p>Income</p>
+            </div>
+            <div className='info__part_value'>
+              <p className='margin-bottom'>0</p> 
+              <p>0</p>
+            </div>
+          </div>
+      </section>
+      <button className="button">Start mining</button>
+
+      <h1>Last blocks</h1>
       <List items={data} onItemClick={openModal} />
       {selectedItem && <Modal item={selectedItem} onClose={closeModal} />}
     </div>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,24 +1,26 @@
+import styles from './List.module.css';
+
 type Item = {
-    id: number;
-    title: string;
-    description: string;
-  };
-  
-  type ListProps = {
-    items: Item[];
-    onItemClick: (item: Item) => void;
-  };
-  
-  const List = ({ items, onItemClick }: ListProps) => {
-    return (
-      <ul>
-        {items.map((item) => (
-          <li key={item.id} onClick={() => onItemClick(item)} style={{ cursor: 'pointer' }}>
-            {item.title}
-          </li>
-        ))}
-      </ul>
-    );
-  };
-  
-  export default List;
+  id: number;
+  title: string;
+  description: string;
+};
+
+type ListProps = {
+  items: Item[];
+  onItemClick: (item: Item) => void;
+};
+
+const List = ({ items, onItemClick }: ListProps) => {
+  return (
+    <ul className={styles.list}>
+      {items.map((item) => (
+        <li key={item.id} className={styles.item} onClick={() => onItemClick(item)}>
+          {item.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
